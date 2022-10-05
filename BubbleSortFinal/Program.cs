604fd88e5fe81459a2073eedc033b11183f62655
Thariq Azhar <thariq.azhar.ft21@mail.umy.ac.id>
2022-10-05 18:21:00 +0700

    Method 1

--- BubbleSortFinal/Program.cs
+++ BubbleSortFinal/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        //deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        //fungsi / method untuk menerima masukan
+        public void read()
+        {
+            //menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.WriteLine("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 20 elemen.\n");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("------------------------");
+            Console.WriteLine(" Masukkan elemen array ");
+            Console.WriteLine("------------------------");
+
+            //pengguna memasukkan elemen pada array
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
+
+
+    }
+}
